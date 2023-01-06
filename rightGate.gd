81e035583a3f02311207f4b8439cfc339b5b16fe
Christian Wilson <87831034+ChristivnJvmes@users.noreply.github.com>
2023-01-06 13:50:43 -0600

    Add files via upload

---
+++ rightGate.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+
+var entered = false
+
+func _on_rightGate_body_entered(body: PhysicsBody2D):
+	entered = true
+
+func _on_rightGate_body_exited(body):
+	entered = false
+	
+func _process(delta):
+	if entered == true:
+		get_tree().change_scene("res://rightWorld.tscn")
